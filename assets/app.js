04cd139d9b6d6138e02048f131ab21abed9011c6
AmitSheoran-557 <amit22082006@gmail.com>
2024-09-12 09:23:51 +0530

    first commit

---
+++ assets/app.js
@@ -0,0 +1,28 @@
+let name = prompt("what is your name")
+console.log(name);
+
+// =======================================================
+
+let mode = prompt("select mode from = dark, green, blue ,  ..custom = White");
+let color;
+if (mode === "dark") {
+    console.log(color = "black");
+}
+else if (mode === "green") {
+    console.log(color = "green");
+}
+else if (mode === "blue") {
+    console.log(color = "blue");
+}
+else {
+    console.log(color = "white");
+}
+
+// =============================================================
+
+let age = prompt("what is your age"); {
+    age >= 18 ? "you can vote" : "you CANNOT vote";
+
+    let result = age >= 18 ? "you can vote" : "you CANNOT vote  , you are under age";
+    console.log(result);
+}
