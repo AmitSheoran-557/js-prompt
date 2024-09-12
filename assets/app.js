7cc959873a08a319944305734b7f1785c7dbfacb
AmitSheoran-557 <amit22082006@gmail.com>
2024-09-12 12:58:03 +0530

    let year

--- assets/app.js
+++ assets/app.js
@@ -26,3 +26,28 @@ let age = prompt("what is your age"); {
     let result = age >= 18 ? "you can vote" : "you CANNOT vote  , you are under age";
     console.log(result);
 }
+
+// ===================================================================
+let year;
+let currentYear = new Date().getFullYear()
+switch (currentYear) {
+    case 2020:
+        year = 2020;
+        break;
+    case 2021:
+        year = 2021;
+        break;
+    case 2022:
+        year = 2022;
+        break;
+    case 2023:
+        year = 2023;
+        break;
+    case 2024:
+        year = 2024;
+        break;
+    case 2025:
+        year = 2025;
+        break;
+}
+document.getElementById("demo").innerHTML = " current year is " + year;
